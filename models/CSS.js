4abff0d56a3de737a599f701f8512267a65aad1e
Sam Teel <sjkteel@hotmail.com>
2023-08-03 20:32:57 -0400

    added to models formatting

--- models/CSS.js
+++ models/CSS.js
@@ -5,20 +5,20 @@ class CSS extends Model { }
 
 
 CSS.init(
-    {
-        cheatSheet_id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        CheatSheet_Name: {
-            type: DataTypes.STRING,
-        },
-        CheatSheet_Cheats: {
-            type: DataTypes.STRING,
-        },
-        Creator_id: {
-            type: DataTypes.INTEGER
-        }
+  {
+    cheatSheet_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    CheatSheet_Name: {
+      type: DataTypes.STRING,
+    },
+    CheatSheet_Cheats: {
+      type: DataTypes.STRING,
+    },
+    Creator_id: {
+      type: DataTypes.INTEGER
     }
-)
+  }
+);
--- models/Javascript.js
+++ models/Javascript.js
@@ -5,20 +5,20 @@ class Javascript extends Model { }
 
 
 Javascript.init(
-    {
-        cheatSheet_id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        CheatSheet_Name: {
-            type: DataTypes.STRING,
-        },
-        CheatSheet_Cheats: {
-            type: DataTypes.STRING,
-        },
-        Creator_id: {
-            type: DataTypes.INTEGER
-        }
+  {
+    cheatSheet_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    CheatSheet_Name: {
+      type: DataTypes.STRING,
+    },
+    CheatSheet_Cheats: {
+      type: DataTypes.STRING,
+    },
+    Creator_id: {
+      type: DataTypes.INTEGER
     }
-)
+  }
+);
