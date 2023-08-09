e9e247c030afc53d8cbad2f456a1169b90b05c7e
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-09 12:41:39 -0400

    updated server.js to use sessions

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
-const hbs = exphbs.create({ });
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'keyboard cat',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
@@ -19,5 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 
-
-  app.listen(PORT, () => {console.log(`Now listening ${PORT}`)})
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => { console.log(`Now listening on ${PORT}`) });
+});
