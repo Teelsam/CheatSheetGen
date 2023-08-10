bbf32b1305c2a2d496f8a36ef01b4cb77e6dace1
Kirandeep <kirandeep1195@gmail.com>
2023-08-09 21:17:43 -0400

    adeede route in homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,25 +2,25 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//    // try {
-//       const userData = await User.findAll({
-//          attributes: { exclude: ['password'] },
-//          order: [['username', 'ASC']],
-//       });
+router.get('/', async (req, res) => {
+   // try {
+      const userData = await User.findAll({
+         attributes: { exclude: ['password'] },
+         order: [['username', 'ASC']],
+      });
 
-//       const users = userData.map((project) => project.get({
-//          plain: true
-//       }));
+      const users = userData.map((project) => project.get({
+         plain: true
+      }));
 
-//       res.render('homepage', {
-//          users,
-//          logged_in: req.session.logged_in,
-//       });
+      res.render('homepage', {
+         users,
+         logged_in: req.session.logged_in,
+      });
 //    } catch (err) {
 //       res.status(500).json(err);
 //    }
-// });
+});
 
 
 // router.get('/', (req, res) => {
@@ -30,31 +30,32 @@ const withAuth = require('../utils/auth');
 //    }
 //    res.render('homepage');
 // });
-
-// router.get('/login', (req, res) => {
-//    if (req.session.logged_in) {
-//       res.redirect('/');
-//       return;
-//    }
-
-//    res.render('login');
-// });
-router.get('/', (req, res) => {
-   res.render('homepage', {current_page:"home"});
-
-      res.render('homepage', {
-         users,
-         logged_in: req.session.logged_in,
-      });
-   } 
-)
-router.get('/login', (req, res) => {
-   res.render('login', {current_page:"login"});
-});
-
 router.get('/signup', (req, res) => {
    res.render('signup');
 });
+
+router.get('/login', (req, res) => {
+   if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+   }
+
+   res.render('login');
+});
+// router.get('/', (req, res) => {
+//    res.render('homepage', {current_page:"home"});
+
+//       res.render('homepage', {
+//          users,
+//          logged_in: req.session.logged_in,
+//       });
+//    } 
+// )
+// router.get('/login', (req, res) => {
+//    res.render('login', {current_page:"login"});
+// });
+
+
 
 router.get('/main', (req, res) => {
    res.render('project');
