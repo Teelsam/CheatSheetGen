728a90628d634b61bc74fb884b97294bc6fd2615
Kirandeep <kirandeep1195@gmail.com>
2023-08-10 17:00:09 -0400

    added data in seed file

--- seeds/htmlData.js
+++ seeds/htmlData.js
@@ -6,8 +6,13 @@ const htmlData = [
         CheatSheet_Name: '<h1>',
         CheatSheet_Cheats: "Header elements go from size 1-6",
         User_id: 1
+    },
+    {
+        cheatSheet_id: 2,
+        CheatSheet_Name: '<h1>',
+        CheatSheet_Cheats: "Header elements go from size 1-6",
+        User_id: 1
     }
-];
-
+]
 const seedHtml = () => HTML.bulkCreate(htmlData);
 module.exports = seedHtml;
