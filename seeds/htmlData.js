ce272c970538c5972320dc538ed6ff8923ba5f2f
Sam Teel <sjkteel@hotmail.com>
2023-08-10 19:14:18 -0400

    added my changes to routes, was missing export

--- seeds/htmlData.js
+++ seeds/htmlData.js
@@ -9,9 +9,9 @@ const htmlData = [
     },
     {
         cheatSheet_id: 2,
-        CheatSheet_Name: '<h1>',
-        CheatSheet_Cheats: "Header elements go from size 1-6",
-        User_id: 1
+        CheatSheet_Name: '<title>',
+        CheatSheet_Cheats: "Gives your websites tab a name",
+        User_id: 2
     }
 ]
 const seedHtml = () => HTML.bulkCreate(htmlData);
