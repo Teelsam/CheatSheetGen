f174b1b47b82f582ec78fdec9fc92d6a19257760
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-12 13:55:45 -0400

    removed withauth

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { User, HTML, CSS, Javascript } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
 
    const userData = await User.findAll({
       attributes: { exclude: ['password'] },
