74b37a14d1dff96cb2e27c6bc8fce93d504d83bc
Sam Teel <sjkteel@hotmail.com>
2023-08-12 17:17:09 -0400

    added more data to each seed file

--- seeds/jsData.js
+++ seeds/jsData.js
@@ -2,15 +2,47 @@ const { Javascript } = require('../models');
 
 const jsData = [
     {
-      
+
         CheatSheet_Name: 'let as a var',
         CheatSheet_Cheats: 'let is 1 of 2 variables in Javascript',
         User_id: 1
     },
     {
-       
+
         CheatSheet_Name: 'const',
-        CHeatSheet_Cheats: "const is a variable in javascript which is more strict",
+        CheatSheet_Cheats: "const is a variable in javascript which is more strict",
+        User_id: 1
+    }, {
+        CheatSheet_Name: 'if',
+        CheatSheet_Cheats: "Checks to see if an expression is true",
+        User_id: 1
+    }, {
+        CheatSheet_Name: 'else if',
+        CheatSheet_Cheats: "Checks to see if an expression is true, after the first check failed",
+        User_id: 1
+    }, {
+        CheatSheet_Name: 'else',
+        CheatSheet_Cheats: "Checks to see if an expression is true, as a final option",
+        User_id: 1
+    }, {
+        CheatSheet_Name: 'function ()',
+        CheatSheet_Cheats: "Creates a scoped grouping of code which can be run by calling the function",
+        User_id: 1
+    }, {
+        CheatSheet_Name: 'while loop',
+        CheatSheet_Cheats: "Checks an expression and while it is true, perform specified actions",
+        User_id: 1
+    }, {
+        CheatSheet_Name: 'Switch Statement',
+        CheatSheet_Cheats: "Checks an expression against various cases, which each contain a break. Else runs default",
+        User_id: 1
+    }, {
+        CheatSheet_Name: 'document.getElementById',
+        CheatSheet_Cheats: "Connects html Element to the javascript file",
+        User_id: 1
+    }, {
+        CheatSheet_Name: 'x++',
+        CheatSheet_Cheats: "A variable with ++ increases that variables value by 1",
         User_id: 1
     },
 
--- seeds/userData.js
+++ seeds/userData.js
@@ -10,7 +10,39 @@ const userData = [
         username: "leetFreak",
         email: "11001@gmail.com",
         password: 'password'
-    }
+    }, {
+        username: "keyBoardKing",
+        email: "56563@gmail.com",
+        password: 'password'
+    }, {
+        username: "AppleMouseAficionado",
+        email: "56564@gmail.com",
+        password: 'password'
+    }, {
+        username: "processorProcessing",
+        email: "couldBeBites@gmail.com",
+        password: 'password'
+    }, {
+        username: "TundraTiger",
+        email: "youveGotMail@gmail.com",
+        password: 'password'
+    }, {
+        username: "aSmallTwig",
+        email: "156ThousandTwigs@gmail.com",
+        password: 'password'
+    }, {
+        username: "HairyHarry1",
+        email: "soManyLetters@gmail.com",
+        password: 'password'
+    }, {
+        username: "Dr.Ghost",
+        email: "ghostDoctor@gmail.com",
+        password: 'password'
+    }, {
+        username: "oneOrTwoCats",
+        email: "meow@gmail.com",
+        password: 'password'
+    },
 ];
 
 const seedUser = () => User.bulkCreate(userData);
